461ddda84f0c3eb14139ffb6e570ff5383bf80e2
Haselnussbomber <mail@haselnussbomber.de>
2018-08-21 20:48:21 +0200

    update for BfA

---
+++ core.lua
@@ -0,0 +1,27 @@
+local function hook(self, unit, index, filter)
+	local buffSpellID = select(10, UnitAura(unit, index, filter));
+	if (not buffSpellID) then
+		return;
+	end
+
+	local mountIDs = C_MountJournal.GetMountIDs();
+	for _, mountID in ipairs(mountIDs) do
+		local _, spellID, _, _, _, _, _, _, _, _, isCollected = C_MountJournal.GetMountInfoByID(mountID);
+		if (spellID == buffSpellID) then
+			local _, _, sourceText = C_MountJournal.GetMountInfoExtraByID(mountID);;
+			self:AddLine("\n|cffA3C3F0MountInfo|r", 1, 1, 1);
+			if (isCollected) then
+				self:AddLine(COLLECTED, RED_FONT_COLOR.r, RED_FONT_COLOR.g, RED_FONT_COLOR.b);
+			else
+				self:AddLine(NOT_COLLECTED, GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b);
+			end
+			self:AddLine(sourceText, 1, 1, 1);
+			self:Show();
+			break
+		end
+	end
+end
+
+hooksecurefunc(GameTooltip, "SetUnitAura", hook);
+hooksecurefunc(GameTooltip, "SetUnitBuff", hook);
+hooksecurefunc(GameTooltip, "SetUnitDebuff", hook);
